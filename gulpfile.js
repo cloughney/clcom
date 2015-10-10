9835b6b9a6c7de1430ad37d4cd3f6ca4c5fba070
= <chris@chrisloughney.com>
2015-10-10 16:47:28 -0700

    added js/css file versioning to HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//TODO make a gulp config file
+
 var styleDir = "./style/**/*.scss";
 
 var gulp = require('gulp');
@@ -7,19 +9,9 @@ var tsc = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 var rjsOptimize = require('gulp-requirejs-optimize');
 var sass = require('gulp-sass');
+var replace = require('gulp-replace');
 
 var tsProject = tsc.createProject("tsconfig.json");
-
-gulp.task("requirejs-optimize", ["build-ts"], function () {
-	return gulp.src("./www/app/bootstrap.js")
-		.pipe(rjsOptimize({
-			paths: {
-				"jquery": "empty:",
-				"knockout": "empty:"
-			}
-		}).on("error", function(error) { console.error(error.message); }))
-		.pipe(gulp.dest("./www/app"));
-});
 
 gulp.task("build-ts", function () {
 	var result = tsProject.src()
@@ -33,6 +25,17 @@ gulp.task("build-ts", function () {
 		.pipe(gulp.dest("./www"));
 });
 
+gulp.task("requirejs-optimize", ["build-ts"], function () {
+	return gulp.src("./www/app/bootstrap.js")
+		.pipe(rjsOptimize({
+			paths: {
+				"jquery": "empty:",
+				"knockout": "empty:"
+			}
+		}).on("error", function(error) { console.error(error.message); }))
+		.pipe(gulp.dest("./www/app"));
+});
+
 gulp.task("compile-sass", function () {
     return gulp.src(styleDir)
         .pipe(sass({ outputStyle: "compressed" })
@@ -40,10 +43,22 @@ gulp.task("compile-sass", function () {
         .pipe(gulp.dest("./www/css"));
 });
 
+gulp.task("version-files", function () {
+	var buildVersion = (function (prevBuildVer) {
+		return (new Date()).getTime();
+	})();
+
+	return gulp.src(["./www/index.html"])
+		.pipe(replace(/(<link\s.*href=)"(css\/[a-z-_\/\.]+\.css(\?v=(\d+))?)"/gi, '$1"$2?v=' + buildVersion + '"'))
+		.pipe(replace(/(<script\s.*src=)"(app\/[a-z-_\/\.]+\.js(\?v=(\d+))?)"/gi, '$1"$2?v=' + buildVersion + '"'))
+		.on("error", function () { console.log(error.message); })
+		.pipe(gulp.dest("./www"));
+});
+
 gulp.task("watch-ts", function () { gulp.watch(tsProject.options.files, ["build-ts"]); });
 gulp.task("watch-sass", function () { gulp.watch(styleDir, ["compile-sass"]); });
 gulp.task("watch", ["watch-ts", "watch-sass"], function() {});
 
-gulp.task("default", ["requirejs-optimize", "compile-sass"], function () {
+gulp.task("release", ["requirejs-optimize", "compile-sass", "version-files"], function () {});
 
-});
+gulp.task("default", function () {});
