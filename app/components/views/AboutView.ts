a4069aaf0f3679393ff24d2df54c341f26ade850
= <chris@chrisloughney.com>
2015-10-10 16:47:28 -0700

    added js/css file versioning to HTML

--- app/components/views/AboutView.ts
+++ app/components/views/AboutView.ts
@@ -17,9 +17,9 @@ class AboutView {
     public input: KnockoutObservable<string>;
     public output: KnockoutObservable<string>;
 
-    public onKeyPress(model: AboutView, e: KeyboardEvent): boolean {
-        var keyCode = e.which || e.keyCode || -1;
-
+    /** Handles the 'onkeydown' event and handles command keypresses */
+    public onKeyDown(model: AboutView, e: KeyboardEvent): boolean {
+        var keyCode = e.keyCode || e.which || -1;
         if (keyCode === -1) { return true; } //TODO display error message on blur div
 
         switch (keyCode) {
@@ -37,12 +37,52 @@ class AboutView {
             case 40:
                 return;
             default:
+                if (keyCode >= 33 && keyCode <= 126 && !e.shiftKey) {
+                    return false;
+                }
+
                 console.log(keyCode);
                 break;
         }
 
-        var input = model.input() + String.fromCharCode(keyCode);
-        model.input(input);
+        return true;
+    }
+
+    /** Handles the 'onkeypress' event and appends visible characters to the input */
+    public onKeyPress(model: AboutView, e: KeyboardEvent): boolean {
+        var charCode = e.keyCode || e.which || -1;
+        if (charCode === -1) { return true; } //TODO display error message on blur div
+
+        // switch (keyCode) {
+        //     case 8: //backspace
+        //         var input = model.input();
+        //         input = input.substr(0, input.length - 1);
+        //         model.input(input);
+        //         return false;
+        //     case 13:
+        //         model.handleCommand(model.input());
+        //         return false;
+        //     case 37: //arrow keys
+        //     case 38:
+        //     case 39:
+        //     case 40:
+        //         return;
+        //     default:
+        //         if (keyCode >= 33 && keyCode <= 126 && !e.shiftKey) {
+        //             return false;
+        //         }
+        //
+        //         console.log(keyCode);
+        //         break;
+        // }
+
+        console.log(charCode + "");
+
+        if (charCode >= 33 && charCode <= 126 && !e.shiftKey) {
+            var char = String.fromCharCode(charCode);
+            model.input(model.input() + char);
+            return false;
+        }
 
         return true;
     };
